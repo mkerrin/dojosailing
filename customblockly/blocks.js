2a044fdd8deaca624d9ebd192d931cab559e9dc8
Michael Kerrin <michael.kerrin@hp.com>
2014-04-11 12:56:25 +0100

    Improvements from Marcus to blocks
    * Adds boat speed option
    
    * Adds UI to select angle

--- customblockly/blocks.js
+++ customblockly/blocks.js
@@ -1,28 +1,42 @@
-Blockly.Blocks['maze_turn'] = {
+Blockly.Blocks['boat_turn'] = {
     // Block for turning left or right.
     init: function() {
         var DIRECTIONS =
             [
                 // [BlocklyApps.getMsg('Maze_turnLeft'), 'turnLeft'],
-                ["Turn left", 'turnLeft'],
-                [BlocklyApps.getMsg('Maze_turnRight'), 'turnRight']
+                ["left", 'turnLeft'],
+                ["right", 'turnRight']
             ];
-        if (Custom.addArrows) {
-            // Append arrows to direction messages.
-            DIRECTIONS[0][0] += ' \u27F2';
-            DIRECTIONS[1][0] += ' \u27F3';
-        }
+
         this.setHelpUrl('http://code.google.com/p/blockly/wiki/Turn');
-        this.setColour(290);
+        this.setColour(200);
         this.appendDummyInput()
-            .appendField(new Blockly.FieldDropdown(DIRECTIONS), 'DIR');
+            .appendField(new Blockly.FieldLabel('turn'))
+            .appendField(new Blockly.FieldDropdown(DIRECTIONS), 'DIR')
+            .appendField(new Blockly.FieldLabel('by'))
+            .appendField(new Blockly.FieldAngle(), 'ANGLE');
         this.setPreviousStatement(true);
         this.setNextStatement(true);
         this.setTooltip(BlocklyApps.getMsg('Maze_turnTooltip'));
     }
 };
 
-Blockly.JavaScript['maze_turn'] = function(block) {
+Blockly.Blocks['boat_speed'] = {
+    // Block for turning left or right.
+    init: function() {
+        this.setHelpUrl('http://code.google.com/p/blockly/wiki/Turn');
+        this.setColour(150);
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldLabel('set speed to'))
+            .appendField(new Blockly.FieldTextInput('0', Blockly.FieldTextInput.numberValidator), 'SPEED')
+            .appendField(new Blockly.FieldLabel('%'));
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+        this.setTooltip(BlocklyApps.getMsg('Maze_turnTooltip'));
+    }
+};
+
+Blockly.JavaScript['boat_turn'] = function(block) {
     // Generate JavaScript for turning left or right.
     var dir = block.getFieldValue('DIR');
     return 'Maze.' + dir + '(\'block_id_' + block.id + '\');\n';
